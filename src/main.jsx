5d7d2824bfac959f68b94d78397177f3f32383b5
Adarsh Patil <156649530+qasgon@users.noreply.github.com>
2026-07-25 02:51:39 +0530

    Revert "Routing, Protected Routes and Layouts Creation"

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { AuthProvider } from './context/AuthContext'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
@@ -10,10 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
         <App />
         <ToastContainer position="top-right" autoClose={3000} />
-      </AuthProvider>
+      
     </BrowserRouter>
   </React.StrictMode>
 )
